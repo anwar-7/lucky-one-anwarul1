58f809145076b8e7e738af30707b8c5f079a4c9c
Anwar <96816038+anwarul1@users.noreply.github.com>
2022-03-26 00:57:40 +0600

    shop component added in app

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        <h3>This is Shop</h3>
+      </div>
+      <div className="cart-container">
+        <h3>Selected Item</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
